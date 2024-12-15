d4fd678e2dfc7227036eee36ef93495ce38c2f27
Betomp28 <157768121+Betomp28@users.noreply.github.com>
2024-12-15 13:04:47 -0600

    Pruevas-testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   projectId: '46qpen',
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
